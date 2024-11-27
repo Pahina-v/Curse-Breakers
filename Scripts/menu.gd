2bff9ba679e665062bfb60917915fe0b4f7f46e4
Pahina.v <maks.dzialowski@gmail.com>
2024-11-27 22:26:14 +0100

    minor fix
    you can now start the game using controller

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 func _on_graj_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main.tscn")
-
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed('cont_accept'):
+		get_tree().change_scene_to_file("res://Main.tscn")
 
 func _on_menu_finished() -> void:
 	$CanvasLayer/Menu.play()
