b9e1862d4a23cf502898262dfb2fda6635474620
Pahina.v <maks.dzialowski@gmail.com>
2024-11-27 20:34:15 +0100

    minor fixes and controller support
    added controller support, deleted the quit button becaused it bugged the game on web build, deleted the tutorial from level 0 and added it to the main menu

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -5,9 +5,5 @@ func _on_graj_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main.tscn")
 
 
-func _on_wyjdz_pressed() -> void:
-	get_tree().quit()
-
-
 func _on_menu_finished() -> void:
 	$CanvasLayer/Menu.play()
