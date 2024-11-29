5aabddcd103ffce065e256f03d1a2a415922e975
Pahina.v <maks.dzialowski@gmail.com>
2024-11-29 16:28:08 +0100

    timer update
    made the last timer show minutes

--- Scripts/8.gd
+++ Scripts/8.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 func _ready() -> void:
 	get_tree().paused = true
+	var minuty = int(Global.time) / 60
+	var sekundy = float(Global.time) - 60.0 * minuty
 	$CanvasLayer/RichTextLabel.text = '
 	
 [center]YOU BROKE THE CURSE
 YOU DIED [color=red] ' + str(Global.deaths) +' [/color] TIMES
-WITH A FINAL TIME OF [color= green] '+str(Global.time)+'s [/color]
+WITH A FINAL TIME OF [color= green] '+str(minuty) + 'min ' + str(sekundy) +'s [/color]
 THANKS FOR PLAYING THE DEMO
 MADE BY
 MAKS DZIALOWSKI
